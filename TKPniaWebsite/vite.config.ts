e6de3d8f24751d63422b44f0e33dca92a89574d4
GreyStormStudio <2300706565@qq.com>
2025-04-23 22:29:53 +0800

    无用注释删除

--- TKPniaWebsite/vite.config.ts
+++ TKPniaWebsite/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 assetFileNames: (assetInfo) => {
-                    if (assetInfo.name?.includes('.svg'))
+                    if (assetInfo.names?.includes('.svg'))
                         return 'assets/[name].[hash].[ext]'
                     return '[name].[hash].[ext]'
                 }
@@ -27,15 +27,11 @@ export default defineConfig({
         }
     },
     server: {
-        // 代理配置，用于重定向请求到其他服务器
         proxy: {
-          // 定义一个代理规则，将/hello-world路径下的请求代理到指定的目标服务器
           '/': {
-            // 目标服务器的地址
             target: 'https://tkpniadevelopmentdepartment.github.io/TKPnia-Shit-Production-Department/',
             // 更改请求的origin为代理服务器的origin，以便与目标服务器交互
             changeOrigin: true,
-            // 重写请求路径，移除/hello-world前缀
             rewrite: (path) => path.replace(/^\//, '')
           }
         }
