d695422097aa7e976ce28102fe2d10d078c10bb7
GreyStormStudio <2300706565@qq.com>
2025-04-24 22:42:52 +0800

    feat: 添加任某人图片并调整样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,19 @@
     <div class="home">
         <h1>Welcome to the TKPNIA Shit Production Department</h1>
         <h2><s>欢迎来到TKPnia产屎部</s></h2>
+        <h3><s>↓任某人镇楼↓</s></h3>
+        <img src="../../public/rmr.png" class="rmr"/>
     </div>
 </template>
 <script></script>
 <style>
 .home {
     text-align: center;
+}
+/* 图片居中 */
+.rmr {
+    display: block;
+    margin: 0 auto;
+    width: 50%;
 }
 </style>
