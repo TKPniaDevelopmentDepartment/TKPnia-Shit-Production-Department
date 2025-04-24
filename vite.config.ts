8a3c29c826a5b429ba3149f851b1eba8c6b4962a
Highmore <106297608+Fredrick-LX@users.noreply.github.com>
2025-04-24 09:29:54 +0800

    修改文件结构

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vite.dev/config/
+export default defineConfig({
+    base: './', // 本地调试必须设置
+    publicDir: 'public', // 默认配置
+    plugins: [vue()],
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets', // 保持默认
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.names?.includes('.svg'))
+                        return 'assets/[name].[hash].[ext]'
+                    return '[name].[hash].[ext]'
+                }
+            }
+        }
+    },
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    server: {
+        proxy: {
+          '/': {
+            target: 'https://tkpniadevelopmentdepartment.github.io/TKPnia-Shit-Production-Department/',
+            // 更改请求的origin为代理服务器的origin，以便与目标服务器交互
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\//, '')
+          }
+        }
+      }
+})
