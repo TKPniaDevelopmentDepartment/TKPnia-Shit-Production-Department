97a8c5ef87050843512aec49fb27b5879b5159bc
GreyStormStudio <2300706565@qq.com>
2025-04-25 14:01:41 +0800

    fix:修改吉祥物的图片，并更新图片显示格式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,28 @@
 <template>
     <div class="home">
-        <h1>Welcome to the TKPNIA Shit Production Department</h1>
-        <h2><s>欢迎来到TKPnia产屎部</s></h2>
-        <h3><s>↓任某人镇楼↓</s></h3>
-        <img src="/rmr.svg" class="rmr"/>
+        <h1 class="title">Welcome to the TKPNIA Shit Production Department</h1>
+        <h2 class="title"><s>欢迎来到TKPnia产屎部</s></h2>
+        <h3 class="title"><s>↓任某人镇楼↓</s></h3>
+        <div class="image_containter">
+            <img src="/rmr.svg" class="rmr" />
+        </div>
     </div>
 </template>
 <script></script>
 <style>
-.home {
+.title {
+    font-weight: bold;
     text-align: center;
 }
-/* 图片居中 */
+
+/* 超出图片容器的部分隐藏,避免遮挡文字 */
+.image_containter {
+    overflow: hidden;
+}
+
 .rmr {
     display: block;
-    margin: 0 auto;
-    width: 50%;
+    margin: -10% auto;
+    width: 50%;/* 原始图片大小的25% */
 }
 </style>
